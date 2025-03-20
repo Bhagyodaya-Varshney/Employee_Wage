530c1c5660d9c4fa785281ef180993f1e7d0e4d4
Bhagyodaya_Varshney <mtrbhagyodayame@email.com>
2025-03-20 15:30:23 +0530

    UC-4 Emp Wages Per Month DONE

--- Employee_Wage.js
+++ Employee_Wage.js
@@ -8,8 +8,6 @@ const FULL_TIME_HOURS = 8;
 
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
 
 function getWorkingHours(empCheck) {
     switch (empCheck) {
@@ -24,6 +22,11 @@ function getWorkingHours(empCheck) {
     }
   }
 
-  empHrs = getWorkingHours(empCheck);
-  let empWage = empHrs * WAGE_PER_HOUR;
-  console.log("Emp Wage: " + empWage);
+  const NUM_OF_WORKING_DAYS = 2;
+  let empHrs = 0;
+  for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+    let empCheck = Math.floor(Math.random() * 10) & 3;
+    empHrs += getWorkingHours(empCheck);
+  }
+  let empwage = empHrs * WAGE_PER_HOUR;
+  console.log("Total Hrs: " + empHrs + " Emp Wage: " + empwage);
